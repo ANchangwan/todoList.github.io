33f256e4876ee445271f3a28486c4862d7910208
안창완 <ssdii66@gmail.com>
2024-06-30 00:49:01 +0900

    img 수정

--- js/background.js
+++ js/background.js
@@ -3,4 +3,4 @@ const body = document.querySelector("body");
 const imgs = ["1.jpg", "2.jpg", "3.jpg"];
 const randomImg = Math.floor(Math.random() * imgs.length);
 
-body.style.backgroundImage = `url(/img/${imgs[randomImg]})`;
+body.style.backgroundImage = `url(img/${imgs[randomImg]})`;
